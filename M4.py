1c768f6a6e81c75d42d5dd2da2898a68ca930e5c
Shohei <maruyama@a.phys.nagoya-u.ac.jp>
2015-09-27 21:18:42 +0900

    Update M4.py

--- M4.py
+++ M4.py
@@ -36,7 +36,7 @@ class mirror_controller(object):
 			nstep = -60500
 		else:
 			nstep = 60500
-		status = self.dio.ctrl.get_count()
+		status = self.dio.ctrl.get_counter()
 		if status:
 			if status == 0x0004:
 				pos = 'smart'
