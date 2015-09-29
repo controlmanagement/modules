d77903467ee7efcb4ea1765200b0952ff7b5b094
H.Iwamura <h.iwamura@a.phys.nagoya-u.ac.jp>
2015-09-29 19:05:12 +0900

    Create antenna_enc.py

---
+++ antenna_enc.py
@@ -0,0 +1,47 @@
+import math
+import numpy
+import pyinterface
+
+
+class enc_controller(object):
+	
+	Az = ''
+	El = ''
+	
+	
+	
+	
+	def __init__(self):
+		self.dio = pyinterface.create_gpg6204(1)
+		pass
+	
+	def print_msg(self, msg):
+		print(msg)
+		return
+	
+	def print_error(self, msg):
+		self.error.append(msg)
+		self.print_msg('!!!! ERROR !!!! ' + msg)
+		return
+	
+	def get_azel(self, ):
+		
+		
+		
+		
+		
+		
+		cnt_Az = self.dio.
+		cnt_El = self.dio.
+		
+		if cnt El > 0:
+			enc_El = (324*cnt_El+295)/590
+		else:
+			enc_El = (324*cnt_El-295)/590
+		self.El = enc_El+45*3600
+		
+		
+		
+
+	def read_azel(self):
+		return [self.Az, self.El]
