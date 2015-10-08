48045ba384fb9c32f05b64fcb25e5a51868bc991
H.Iwamura <h.iwamura@a.phys.nagoya-u.ac.jp>
2015-10-08 11:17:44 +0900

    Update coord.py

--- coord.py
+++ coord.py
@@ -1,6 +1,6 @@
 import math
 import ctypes
-import lib_sla as sla
+import slalib as sla
 import lib_jpl as jpl
 
 
@@ -53,67 +53,91 @@ class coord_calc(object):
 		
 		if err_code == False:
 			dist = math.sqrt(r[0] * r[0] + r[1] * r[1] + r[2] * r[2])
-			jpl_pleph(ephem, jd, nctr, 12, r1, 1)                                #Earth position at jd
-			jpl_pleph(ephem, jd - dist * tau / (24. * 3600.), ntarg, 12, r2, 1)  # Target position when the light left 
+			jpl.jpl_pleph(ephem, jd, nctr, 12, r1, 1)                                #Earth position at jd
+			jpl.jpl_pleph(ephem, jd - dist * tau / (24. * 3600.), ntarg, 12, r2, 1)  # Target position when the light left 
 			for i in range(6):
 				rr[i] = r2[i] - r1[i]
 			dist = math.sqrt(rr[0] * rr[0] + rr[1] * rr[1] + rr[2] * rr[2])
-			ra = self._double_p(0)
-			dec = self._double_p(0)
-			sla.slaDcc2s(rr, ra, dec)
-			
+			ret = sla.slaDcc2s(rr)
+			ra = ret[0]
+			dec = ret[1]
 			ra = sla.slaDranrm(ra)
 			radi = math.asin(self.eqrau[ntarg] / (dist.value * aukm))
-			return [ra.value, dec.value, dist, radi]
+			return [ra, dec, dist, radi]
 		else
 			return err_code
 
-	def planet_J2000_geo_to_topo(gra, gdec, *dist, *radi, *tra, *tdec, jd_utc, dut1, tai_utc, longitude, latitude, height):
+	def planet_J2000_geo_to_topo(gra, gdec, dist, radi, jd_utc, dut1, tai_utc, longitude, latitude, height):
+		date = jd_utc - 2400000.5 + dut1 / (24. * 3600.)
+		jd = jd_utc - 2400000.5 + (tai_utc + 32.184) / (24. * 3600.)
 		
-  double vgp[6], stl, vgo[6], v[6], rmat[3][3];
-  double date = jd_utc - 2400000.5 + dut1 / (24. * 3600.);
-  double jd = jd_utc - 2400000.5 + (tai_utc + 32.184) / (24. * 3600.);
-  double disttmp;
+		# Spherical to x,y,z 
+		v = sla.slaDcs2c(gra, gdec)
+		for i in range[3]:
+			v[i] *= dist
+  		
+		# Precession to date. 
+		rmat = sla.slaPrec(2000.0, sla.slaEpj(jd))
+		vgp = sla.slaDmxv(rmat, v)
+		
+		# Geocenter to observer (date). 
+		stl = sla.slaGmst(date) + longitude
+		vgo = sla.slaPvobs(latitude, height, stl)
+		
+		# Observer to planet (date). 
+		for i in range (6):
+			v[i] = vgp[i] - vgo[i]
+		
+		disttmp = *dist
+		dist = math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2])
+		radi *= disttmp / dist
+		
+		# Precession to J2000 
+		rmat = sla.slaPrec(sla.slaEpj(jd), 2000.)
+		vgp = sla.slaDmxv(rmat, v)
+		
+		# To RA,Dec. 
+		ret = sla.slaDcc2s(vgp)
+		tra = sla.slaDranrm(ret[0])
+		tdec = ret[1]
+		return [dist, radi, tra, tdec]
 
-  // Spherical to x,y,z 
-  slaDcs2c ( gra, gdec, v );
-  for(i = 0; i < 3; i++) v[i] *= *dist;
-  
-  // Precession to date. 
-  slaPrec ( 2000.0, slaEpj(jd), rmat );
-  slaDmxv ( rmat, v, vgp );
+	def apply_kisa(self, double az1, double el1, double *az2, double *el2):
+  double cos_az,sin_az,cos_el,sin_el,
+    d_az,	/* offset of Az origin */
+    d_el;	/* offest of El origin */
+  		
+		# calculate the values of correction
+		cos_az = math.cos(az1)
+		sin_az = math.sin(az1)
+		cos_el = math.cos(el1)
+		sin_el = math.sin(el1)
+		
+		# basic correction for AzEl mount
+		d_az = gkisa.a1*sin_el + gkisa.a2 + gkisa.a3*cos_el +gkisa.b1*sin_az*sin_el - gkisa.b2*cos_az*sin_el;
+  d_el =  gkisa.b1*cos_az + gkisa.b2*sin_az + gkisa.b3 + gkisa.g1 * el1 * 180. / M_PI;
 
-  // Geocenter to observer (date). 
-  stl = slaGmst ( date ) + longitude;
-  slaPvobs ( latitude, height, stl, vgo );
-  
-
-  // Observer to planet (date). 
-  for ( i = 0; i < 6; i++ ) {
-    v[i] = vgp[i] - vgo[i];
+  /* For radio observations */
+  if(gopt_flag == 0) {
+    d_az=d_az
+      + gkisa.c1*sin(az1-el1) + gkisa.c2*cos(az1-el1)
+      + gkisa.d1
+      + gkisa.e1*cos_el - gkisa.e2*sin_el;
+    d_el=d_el
+      + gkisa.c1*cos(az1-el1) - gkisa.c2*sin(az1-el1)
+      + gkisa.d2
+      + gkisa.e1*sin_el + gkisa.e2*cos_el;
   }
+  /* convert to encoder offset on the horizon */
+  d_az =  d_az / cos_el;
 
-  disttmp = *dist;
-  *dist = sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
-  *radi *= disttmp / *dist;
-
-  // Precession to J2000 
-  slaPrec ( slaEpj(jd) , 2000., rmat );
-  slaDmxv ( rmat, v, vgp );
-
-  
- // To RA,Dec. 
-  slaDcc2s ( vgp, tra, tdec );
-  *tra = slaDranrm ( *tra );
+  /* apply the correction values ->  radians*/
+  *az2 = az1 +(d_az /60.0) * M_PI / 180.;
+  *el2 = el1 +(d_el /60.0)* M_PI / 180.;
 }
-
-
-
-
-
-
-
-	def apply_kisa(self, ):
+		
+		
+		
 		
 		
 		
@@ -291,4 +315,226 @@ class coord_calc(object):
 			return lst;
    	 }
 		//return vobs;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	def tracking_proc()
+{
+  double dum1, dum2, dum3;
+  double gxx, gyy, lambda;
+  double planet_ra, planet_dec;
+  struct timeval tv;
+  static double pre_az=0, pre_el=0;
+  static double pre_target_speed_az = 0, pre_target_speed_el = 0, pre_mjd = 0;
+
+  // Calculate current MJD 
+  gettimeofday(&tv, NULL);
+  gmjd = (tv.tv_sec + tv.tv_usec/1000000.)/24./3600. + MJD0;
+  
+  // If radio source or planet, the proper motion sets to zero 
+  if(gopt_flag == 0 || gplanet_flag == 1){
+    gpx = 0.0; gpy = 0.0;
+  }
+
+  if(gplanet_flag == 1){
+    gcoord_mode_flag = COORD_J2000;
+    // Calculate geocentric J2000 coordinate 
+    if(0 != calc_planet_coordJ2000_geocent(ephem, gmjd+2400000.5, tai_utc, gplanet_number, &planet_ra, &planet_dec, &gplanet_dist, &gplanet_radi)){
+    fprintf(stderr, "Cannot calculate the position!\n");
+      exit(1);
+    }
+    planet_J2000_geo_to_topo(planet_ra, planet_dec, &gplanet_dist, &gplanet_radi, &gx, &gy, gmjd+2400000.5, gdut1, tai_utc, glongitude, glatitude, gheight);
+  }
+
+  // Apply offset when the offset coordinate system is the same as that of the source 
+  // gx -> gxx, gy -> gyy 
+  if(goffset_mode_flag == COORD_SAME || goffset_mode_flag == gcoord_mode_flag){
+    gyy = gy + goffy;
+    gxx = (goffdcos == 0)? (gx + goffx): (gx + goffx / cos(gyy));
+        //printf("%f %f %f %f\n", gx, gy, gxx, gyy);
+  }else if(goffset_mode_flag != COORD_J2000){
+    // THIS PART IS NOT IMPLIMENTED YET 
+    gxx = gx; gyy = gy;
+  }
+  // =================== added by nishimura 2011/11/23
+  else
+    {
+     gxx = gx; gyy = gy;
+   }
+  // ------------------- END
+
+  // Convert to J2000
+  if(gcoord_mode_flag == COORD_J2000){
+    gaJ2000 = gxx; gdJ2000 = gyy;
+    gpaJ2000 = gpx; gpdJ2000 = gpy;
+  }else if(gcoord_mode_flag == COORD_B1950){
+    slaFk425(gxx, gyy, gpx, gpy, 0, 0, &gaJ2000, &gdJ2000, &gpaJ2000, &gpdJ2000, &dum1, &dum2);
+    //printf("%f %f %f %f\n", gxx, gyy,gaJ2000, gdJ2000);
+  }else if(gcoord_mode_flag == COORD_LB){
+    // Ignore proper motion in this case 
+    slaGaleq(gxx, gyy, &gaJ2000, &gdJ2000);
+    gpaJ2000 = 0; gpdJ2000 = 0;
+  }
+
+  // Apply offset when the offset coordinate system is not the same as that of the source, 
+  // and the offset coordinate system is J2000 
+  if(!(goffset_mode_flag == COORD_SAME || goffset_mode_flag == gcoord_mode_flag) && goffset_mode_flag == COORD_J2000){
+    gdJ2000 += goffy;
+    gaJ2000 = (goffdcos == 0)? (gaJ2000 + goffx): (gaJ2000 + goffx / cos(gdJ2000));
+  }
+
+
+  // From J2000 to Apparent 
+  if(gcoord_mode_flag != COORD_APP){
+    slaMap(gaJ2000, gdJ2000, gpaJ2000, gpdJ2000, 0.0, 0.0, 2000.0, gmjd + (tai_utc + 32.184)/(24.*3600.), &gaApparent, &gdApparent);
+  }else{
+    // In this case, the proper motion is ignored 
+    gaApparent = gx;
+    gdApparent = gy;
+  }
+  
+  // Apply offset when the offset coordinate system is apparent. 
+  if(goffset_mode_flag == COORD_APP){
+    gpdJ2000 += goffy;
+    gpaJ2000 = (goffdcos == 0)? (gpaJ2000 + goffx): (gpaJ2000 + goffx / cos(gpdJ2000));
+  }
+
+  if(gopt_flag == 1)
+    lambda = glambda_opt;
+  else
+    lambda = glambda_radio;
+
+  // From apparent to Horizontal 
+  slaAop(gaApparent, gdApparent, gmjd, gdut1, glongitude, glatitude, gheight, 0, 0, gtemperature, gpressure, ghumidity, lambda, gtlr, &greal_az, &greal_el, &dum1, &dum2, &dum3);
+  
+  // From zenith angle to elevation 
+  greal_el = M_PI / 2. - greal_el;
+  //greal_az = greal_az + M_PI;
+
+  // Apply horizontal offset. 
+  greal_el += goffel;
+  greal_az = (goffazeldcos == 0)? (greal_az + goffaz): (greal_az + goffaz / cos(greal_el));
+
+  if(instruction == SET_COORD_HORIZONTAL){
+    gtarget_az = greal_az; gtarget_el = greal_el;
+  }else{
+    apply_kisa(greal_az, greal_el, &gtarget_az, &gtarget_el);
+  }
+
+  if(gtarget_az*180./M_PI - angle_az > 180. && gtarget_az*180./M_PI - 360. > AZ_LIMIT_MIN)
+    gtarget_az -= 2*M_PI;
+  else if(gtarget_az*180./M_PI - angle_az < -180. && gtarget_az*180./M_PI + 360. < AZ_LIMIT_MAX)
+    gtarget_az += 2*M_PI;
+
+  if(target_changed == 1){
+    pre_az = gtarget_az; pre_el = gtarget_el; pre_mjd = gmjd;
+    target_changed = 0;
+  }else{
+    gtarget_speed_az = (gtarget_az - pre_az) / (gmjd - pre_mjd) * 180. / M_PI / (24.*3600.);  // degree/sec 
+    gtarget_speed_el = (gtarget_el - pre_el) / (gmjd - pre_mjd) * 180. / M_PI / (24.*3600.);  // degree/sec 
+    pre_az = gtarget_az; pre_el = gtarget_el; pre_mjd = gmjd;
+	//gtarget_az=gtarget_az*180./M_PI;
+	//gtarget_el=gtarget_el*180./M_PI;
+    
+    //||||||||||||||||||||||||||||||||||||||||||||||||
+    printf("(%lf,%lf)->(%lf,%lf)->J2000(%lf,%lf)->App(%lf,%lf)->real(%lf,%lf)\n",gx,gy,gxx,gyy,gaJ2000,gdJ2000,gaApparent,gdApparent,greal_az,greal_el);
+    //||||||||||||||||||||||||||||||||||||||||||||||||
+
+
+  }
+}
+
+
+
+
+
+
+
+
+
+	def read_kisa_file():
+{
+  FILE *fp;
+  int i_prm;
+  char buffer[BUFFERSIZE], *file_stat;
+
+  if(NULL == (fp = fopen(KISA_FILE, "r"))){
+    return -1;
+  }
+  
+  i_prm=0;
+  while(1) {
+    file_stat=fgets(buffer,BUFFERSIZE,fp); /* 1st line for comment */
+    if(file_stat==NULL) {
+      fclose(fp);
+      return(-2);
+    }
+    if(buffer[0]=='#' || strncmp(buffer,"/*",2)==0 || strncmp(buffer,"//",2)==0) {
+      continue;
+    } else {
+      switch(i_prm) {
+      case 0:
+		gkisa.a1=atof(buffer);
+		break;
+      case 1:
+		gkisa.a2=atof(buffer);
+		break;
+      case 2:
+		gkisa.a3=atof(buffer);
+		break;
+      case 3:
+		gkisa.b1=atof(buffer);
+		break;
+      case 4:
+		gkisa.b2=atof(buffer);
+		break;
+      case 5:
+		gkisa.b3=atof(buffer);
+		break;
+      case 6:
+		gkisa.c1=atof(buffer);
+		break;
+      case 7:
+		gkisa.c2=atof(buffer);
+		break;
+      case 8:
+		gkisa.d1=atof(buffer);
+		break;
+      case 9:
+		gkisa.d2=atof(buffer);
+		break;
+      case 10:
+		gkisa.e1=atof(buffer);
+		break;
+      case 11:
+		gkisa.e2=atof(buffer);
+		break;
+      case 12:
+		gkisa.g1=atof(buffer);
+		break;
+      }
+      i_prm++;
+      if(i_prm>12){
+		fprintf(stderr, "%f %f %f %f %f %f %f %f %f %f %f %f %f\n", gkisa.a1, gkisa.a2, gkisa.a3, gkisa.b1, gkisa.b2, gkisa.b3, gkisa.c1, gkisa.c2, gkisa.d1, gkisa.d2, gkisa.e1, gkisa.e2, gkisa.g1);
+		break;
+      }
+    }
+  }
+  fclose(fp);
+ 
+  return 0;
+}
+
 
