879d3995e55e63dcf43b7a5498b4a509735956c6
Shohei <maruyama@a.phys.nagoya-u.ac.jp>
2015-10-15 16:45:48 +0900

    Update abs.py

--- abs.py
+++ abs.py
@@ -9,14 +9,14 @@ class abs_controller(object):
 	position = ''
 	
 	
-	def __init__(self, move_org=True):
-		self.dio = pyinterface.create_gpg2000(1)
+	def __init__(self, ndev=1):
+		self.dio = pyinterface.create_gpg2000(ndev)
 		pass
-		
+	
 	def print_msg(self, msg):
 		print(msg)
 		return
-		
+	
 	def print_error(self, msg):
 		self.error.append(msg)
 		self.print_msg('!!!! ERROR !!!! ' + msg)
@@ -32,16 +32,24 @@ class abs_controller(object):
 			self.position = 'MOVE'
 		return
 
-	def move(self, dist, lock=True):
+	def move(self, dist):
 		self.get_pos()
 		if dist == 'IN':
 			self.buff = 0x00
-		else:
+		elif dist == 'OUT':
 			self.buff = 0x01
 		while dist != self.position:
 			self.dio.ctrl.out_byte('FBIDIO_OUT1_8', self.buff)
 			self.get_pos()
 		return
 	
+	def move_r(self):
+		self.move(self, 'IN')
+		return
+	
+	def move_sky(self):
+		self.move(self, 'OUT')
+		return
+		
 	def read_pos(self):
 		return self.position
