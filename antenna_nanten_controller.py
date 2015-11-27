8d905c9a1c8980cdec797c78515b2122dfe311f4
YumikoYamane <yamane.y@a.phys.nagoya-u.ac.jp>
2015-11-27 12:56:21 +0900

    Update antenna_nanten_controller.py

--- antenna_nanten_controller.py
+++ antenna_nanten_controller.py
@@ -3,6 +3,8 @@ import math
 import coord
 from pyslalib import slalib
 import nanten_main_controller
+import pyinterface
+
 
 
 
@@ -57,7 +59,8 @@ class antenna_nanten_controller(object):
 		tv_sec = int(tv)
 		tv_usec = tv - tv_sec
 		mjd = (tv_sec + tv_usec/1000000.)/24./3600. + 40587.0 # 40587.0 = MJD0
-		
+		tai_utc = 36.0 # tai_utc=TAI-UTC  2015 July from ftp://maia.usno.navy.mil/ser7/tai-utc.dat
+
 		# lamda is wavelength(not lambda)
 		if code_mode == "B1950":
 			ret = slalib.sla_fk425(gx, gy, gpx, gpy, 0, 0)
@@ -84,7 +87,7 @@ class antenna_nanten_controller(object):
 			new_ra = ret[0] + off_x/math.cos(new_dec)
 			dcos = 0
 		
-		ret = slalib.sla_aop(ret[0], ret[1], mjd, self.dut1, self.longitude, self.latitude, self.height, 0, 0, temp, pressure, humid, lamda, tlr)
+		ret = slalib.sla_aop(ret[0], ret[1], mjd, self.dut1, self.longitude, self.latitude, self.height, 0, 0, temp, pressure, humid, lamda, tlr=0.0065)
 		"""
 		ret[0] = azimath(radian, N=0, E=90)
 		ret[1] = zenith(radian)
@@ -93,8 +96,8 @@ class antenna_nanten_controller(object):
 		real_az = ret[0]
 		real_el = math.pi/2. - ret[1]
 		
-		self.move_azel(real_az, real_el, dcos, hosei)
-		return
+		track = self.move_azel(real_az, real_el, dcos, hosei)
+		return track
 	
 	def move_lb(self, gx, gy, temp, pressure, humid, lamda, dcos, hosei = 'hosei_230.txt', off_x = 0, off_y = 0):
 		if dcos == 0:
@@ -104,17 +107,17 @@ class antenna_nanten_controller(object):
 			gy += off_y
 			gx = gx+off_x/math.cos(gy)
 		
-		ret = self.sla.slaGaleq(gx, gy)
+		ret = slalib.sla_galeq(gx, gy)
 		"""
 		gaJ2000 = ret[0]
 		gdj2000 = ret[1]
 		gpaJ2000 = gpdJ2000 = 0
 		"""
 		
-		self.move_radec(ret[0], ret[1], 0, 0, "J2000", temp, pressure, humid, lamda, dcos, hosei)
-		return
+		track = self.move_radec(ret[0], ret[1], 0, 0, "J2000", temp, pressure, humid, lamda, dcos, hosei)
+		return track
 	
-	def move_planet(self, ntarg):
+	def move_planet(self, ntarg,code_mode, temp, pressure, humid, lamda, dcos, hosei = 'hosei_230.txt', off_x = 0, off_y = 0):
 		ret = self.coord.calc_planet_coordJ2000(ntarg)
 		if len(ret) == 1:
 			print(ret) #error
@@ -123,7 +126,7 @@ class antenna_nanten_controller(object):
 		ret = self.coord.planet_J2000_geo_to_topo(ret[0], ret[1], ret[2], ret[3], self.dut1, self.longitude, self.latitude, self.height)
 		
 		#ret[2] = ra, ret[3] = dec
-		self.move_radec(ret[2], ret[3], 0, 0)
+		self.move_radec(ret[2], ret[3], 0, 0,code_mode, temp, pressure, humid, lamda, dcos, hosei  , off_x , off_y )
 		return
 	
 	def calc_otf(self, x, y, dcos, coord_sys, dx, dy, dt, n, rampt, delay, temp = 0, pressure = 0, humid = 0, lamda = 0, hosei = 'hosei_230.txt', code_mode = 'J2000'):
@@ -136,14 +139,15 @@ class antenna_nanten_controller(object):
 		Az_track_flag = El_track_flag = 'TRUE'
 		while Az_track_flag == 'TRUE' or El_track_flag == 'TRUE':
 			if coord_sys == 'HORIZONTAL':
-				ret = self.move_azel(start_x, start_y)
+				ret = self.move_azel(start_x, start_y,dcos)
 				Az_track_flag = ret[0]
 				El_track_flag = ret[1]
-			elif coord_sys == 'EQUATORIAL':
+			elif coord_sys == 'EQUATORIAL':				
 				# ret = self.move_radec(start_x, start_y, gpx, gpy, code_mode, temp, pressure, humid, lamda, dcos, hosei)
 				# gpx,gpy => 0 for radio observation or planet observation
 				# change this when you do optical observation
 				ret = self.move_radec(start_x, start_y, 0, 0, code_mode, temp, pressure, humid, lamda, dcos, hosei)
+
 				Az_track_flag = ret[0]
 				El_track_flag = ret[1]
 			else:
@@ -154,7 +158,7 @@ class antenna_nanten_controller(object):
 		self.otf(mjd+delay/24./3600., start_x, start_y, mjd+(delay+total_t)/24./3600., end_x, end_y, dcos, coord_sys, hosei)
 		return
 	
-	def otf(self, mjd_start, start_x, start_y, mjd_end, end_x, end_y, dcos, coord_sys, hosei):
+	def otf(self, mjd_start, start_x, start_y, mjd_end, end_x, end_y, dcos, coord_sys, hosei,temp = 0, pressure = 0, humid = 0, lamda = 0, code_mode = 'J2000'):
 		otf_end_flag = 0
 		
 		while otf_end_flag == 0:
@@ -164,11 +168,11 @@ class antenna_nanten_controller(object):
 				off_y = (end_y - start_y) / (mjd_end - mjd_start) * (mjd - mjd_start) + start_y
 				
 				if coord_sys == 'HORIZONTAL':
-					self.move_azel(off_x, off_y)
+					self.move_azel(off_x, off_y,dcos)
 				elif coord_sys == 'EQUATORIAL':
 					self.move_radec(off_x, off_y, 0, 0, code_mode, temp, pressure, humid, lamda, hosei)
 				elif coord_sys == 'GALACTIC':
-					self.move_lb(off_x, off_y, temp, pressure, humid, lamda)
+					self.move_lb(off_x, off_y, temp, pressure, humid, lamda,dcos)
 			
 			if mjd > mjd_end:
 				otf_end_flag = 1
@@ -182,8 +186,8 @@ def antenna_monitor_client(host, port):
 	client = pyinterface.server_client_wrapper.monitor_client_wrapper(antenna_nanten_controler, host, port)
 	return client
 
-def start_antenna_server(port1 = , port2 = ):
+def start_antenna_server(port1 = 5925, port2 = 5926):
 	antenna = antenna_nanten_controller()
-	server = pyinterface.server_cliet_wrapper.server_wrapper(antena, '', port1, port2)
+	server = pyinterface.server_client_wrapper.server_wrapper(antenna, '', port1, port2)
 	server.start()
 	return server
