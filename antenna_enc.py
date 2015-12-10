96b3e6429ada39e2acaaeb91f39869edd78f220b
H.Iwamura <h.iwamura@a.phys.nagoya-u.ac.jp>
2015-12-10 11:13:59 -0300

    Update antenna_enc.py

--- antenna_enc.py
+++ antenna_enc.py
@@ -10,11 +10,8 @@ class enc_controller(object):
 	El = ''
 	
 	def __init__(self):
+		portio.iopl(3)
 		pass
-	
-	def open(self, ndev = 1):
-		self.dio = pyinterface.create_gpg6204(ndev)
-		return
 	
 	def start_server(self):
 		ret = self.start_enc_server()
@@ -30,7 +27,7 @@ class enc_controller(object):
 		return
 	
 	
-"""
+	"""
 	# for renishaw (Az,El)
 	def get_azel(self):
 		cntAz = self.dio.get_position(2)
@@ -57,34 +54,36 @@ class enc_controller(object):
 		
 		#dioOutputByte(CONTROLER_BASE0,0x03,0x04);
 		#CONRROLER_BASE0 = 0xc000
-		portio.outb(hex(0xc000+0x03), 0x04)
+		portio.outb(0x04, (0x2050+0x03)) # Az
 		
 		time.sleep(3./1000) # need waiting
 		#dioOutputByte(CONTROLER_BASE0,0x03,0x00);
-		portio.outb(hex(0xc000+0x03), 0x00)
+		portio.outb(0x00, (0x2050+0x03))
 		
 		# get data from board
 		for i in range(3):
 			# byte_az[i] = dioInputByte(CONTROLER_BASE0,i);
-			byte_az[i] = portio.inb(hex(0xc000+i))
+			byte_az[i] = portio.inb((0x2050+i))
 			
 			# reverse byte
-			# byte_az[i]=~byte_az[i];
-			byte_az[i] = ~byte_az[i]
+			# byte_az[i]=~byte_az[i];byte[2] is hugou 1keta+7keta suuji
+			byte_az[i] = ~byte_az[i] & 0b11111111
 			
 		self.Az = self.bin2dec_2s_complement(byte_az, 3)
 		
-		cntEl = self.dio.get_position(1)
+		portio.outb(2, 0x2006)
+		cntEl = portio.inl(0x2000)
 		if cntEl > 0:
-			encEl = (324*cntEl+295)/590
+			encEl = int((324*cntEl+295)/590)
 		else:
-			encEl = (324*cntEl-295)/590
+			encEl = int((324*cntEl-295)/590)
 		self.El = encEl+45*3600      #arcsecond
+		
 		return [self.Az, self.El]
 	
 	
 	
-	def bin2dec_2s_complement(byte, nSize):
+	def bin2dec_2s_complement(self, byte, nSize):
 		i = sign = ord = 1
 		abs = 0
 		if nSize == 0:
@@ -95,19 +94,30 @@ class enc_controller(object):
 			sign = -1
 		else:
 			sign = 1
-		
+		"""
+		num = bin(byte[nSize-1])
+		if len(num) == 11:
+			num = num[3:10]
+			byte[nSize-1] = int(num, 2)
+		"""
 		if sign == 1:
 			for i in range(nSize):
+				print('if') # for test
+				print('byte['+str(i)+']='+str(byte[i])) # for test
 				abs += ord*byte[i]
 				ord *= 256
-			abs += ord*byte[nSize-1] &(~0x80)
+			print('abs='+str(abs)) # for test
+			abs += ord*byte[nSize-1] & (~0x80 & 0b01111111)
 			
 		else:
 			for i in range(nSize):
-				abs += ord*(~byte[i])
+				print('else') # for test
+				print('byte['+str(i)+']='+str(byte[i])) # for test
+				abs += ord*(~byte[i] & 0b11111111)
 				ord *= 256
-			abs += ord*((~byte[nSize-1]) &(~0x80))
+				print(abs)
 			abs += 1
+			print('abs='+str(abs)) # for test
 		
 		return abs*sign
 	
@@ -125,7 +135,7 @@ def enc_monitor_client(host, port):
 	client = pyinterface.server_client_wrapper.monitor_client_wrapper(enc_controller, host, port)
 	return client
 
-def start_enc_server(port1 = ????, port2 = ????):
+def start_enc_server(port1 = 9999, port2 = 9999):
 	enc = enc_controller()
 	server = pyinterface.server_client_wrapper.server_wrapper(enc, '', port1, port2)
 	server.start()
