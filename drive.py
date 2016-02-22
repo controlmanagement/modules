8edaabaf302e43cb359847e2286655d05a2dcfa1
yukihyoto <yhyoto@gmail.com>
2016-02-22 18:53:05 +0900

    Update drive.py

--- drive.py
+++ drive.py
@@ -34,9 +34,10 @@ while(1):
 	secofday2 = ntime2.hour*60*60 + ntime2.minute*60 + ntime2.second + ntime2.microsecond*0.000001
 	
 	#mjd,secofday,self.az_encmoni, self.el_encmoni, self.az_targetmoni, self.el_targetmoni, self.az_hensamoni, self.el_hensamoni, self.az_rate, self.el_rate, self.az_targetspeed, self.el_targetspeed, self.current_speed_az, self.current_speed_el
-
+	#mjd,secofday,self.az_encmoni, self.el_encmoni, self.az_targetmoni, self.el_targetmoni, self.az_hensamoni, self.el_hensamoni, self.az_rate, self.el_rate, self.az_targetspeed, self.el_targetspeed, self.current_speed_az, self.current_speed_el ,self.t1_moni ,self.t2_moni ,self.az_pidihensamoni ,self.el_pidihensamoni
+	
 	#log = str(mjd) + ' ' + str(secofday) + ' ' + str(data["command_az"])+ ' ' + str(data["command_el"])+ ' ' + str(data["current_az"]) + ' ' + str(data["current_el"])
-	log = str(mjd) + ' ' + str(secofday) + ' ' + str(secofday2) + str(data[2])+ ' ' + str(data[3])+ ' ' + str(data[0]) + ' ' + str(data[1]) + ' ' + str(data[4]) + ' ' + str(data[5]) + ' ' + str(data[6]) + ' ' + str(data[7])+ ' ' + str(data[8]) + ' ' + str(data[9]) + ' ' + str(data[10]) + ' ' + str(data[11]) + ' ' + str(data[12]) + ' ' + str(data[13])
+	log = str(mjd) + ' ' + str(secofday) + ' ' + str(secofday2) + str(data[2])+ ' ' + str(data[3])+ ' ' + str(data[0]) + ' ' + str(data[1]) + ' ' + str(data[4]) + ' ' + str(data[5]) + ' ' + str(data[6]) + ' ' + str(data[7])+ ' ' + str(data[8]) + ' ' + str(data[9]) + ' ' + str(data[10]) + ' ' + str(data[11]) + ' ' + str(data[12]) + ' ' + str(data[13]) + ' ' + str(data[14]) + ' ' + str(data[15]) + ' ' + str(data[16]) + ' ' + str(data[17])
 
 	f.write(log + "\n")
 	print log
