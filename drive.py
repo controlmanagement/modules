53340ecf05aa0f8993a5ae28fa0d947b2b2d142b
Shohei <maruyama@a.phys.nagoya-u.ac.jp>
2016-02-22 18:48:00 +0900

    Update drive.py

--- drive.py
+++ drive.py
@@ -24,17 +24,19 @@ status = antenna_nanten_controller.antenna_monitor_client('172.20.0.11',8004)
 while(1):
 	# Calculate current MJD
 	tv = time.time()
-	tv_sec = int(tv)
-	tv_usec = tv - tv_sec
-	mjd = (tv_sec + tv_usec/1000000.)/24./3600. + 40587.0 # 40587.0 = MJD0
+	mjd = tv/24./3600. + 40587.0 # 40587.0 = MJD0
 	mid = int(mjd)
 	ntime = dt.now()
 	secofday = ntime.hour*60*60 + ntime.minute*60 + ntime.second + ntime.microsecond*0.000001
 	#data = status.read_status()
 	data = status.read_azel()
-	# mjd,secofday,command[az],command[el],current[az],currennt[el]
+	ntime2 = dt.now()
+	secofday2 = ntime2.hour*60*60 + ntime2.minute*60 + ntime2.second + ntime2.microsecond*0.000001
+	
+	#mjd,secofday,self.az_encmoni, self.el_encmoni, self.az_targetmoni, self.el_targetmoni, self.az_hensamoni, self.el_hensamoni, self.az_rate, self.el_rate, self.az_targetspeed, self.el_targetspeed, self.current_speed_az, self.current_speed_el
+
 	#log = str(mjd) + ' ' + str(secofday) + ' ' + str(data["command_az"])+ ' ' + str(data["command_el"])+ ' ' + str(data["current_az"]) + ' ' + str(data["current_el"])
-	log = str(mjd) + ' ' + str(secofday) + ' ' + str(data[2])+ ' ' + str(data[3])+ ' ' + str(data[0]) + ' ' + str(data[1])
+	log = str(mjd) + ' ' + str(secofday) + ' ' + str(secofday2) + str(data[2])+ ' ' + str(data[3])+ ' ' + str(data[0]) + ' ' + str(data[1]) + ' ' + str(data[4]) + ' ' + str(data[5]) + ' ' + str(data[6]) + ' ' + str(data[7])+ ' ' + str(data[8]) + ' ' + str(data[9]) + ' ' + str(data[10]) + ' ' + str(data[11]) + ' ' + str(data[12]) + ' ' + str(data[13])
 
 	f.write(log + "\n")
 	print log
