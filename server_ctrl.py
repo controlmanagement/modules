e8c1580fc28858c49cd0170c4c8af1749d965958
yukihyoto <yhyoto@gmail.com>
2016-06-10 18:59:21 +0900

    Update server_ctrl.py
    necctrlとgithubのスクリプト統合作業を行った。

--- server_ctrl.py
+++ server_ctrl.py
@@ -16,36 +16,21 @@ ret2 = abs.start_abs_server()
 import time
 import antenna_enc
 import antenna_nanten_controller
-import dome_pos
+#import dome_pos
 import dome
-import weather
 import subprocess as sub
 
 ret3 = antenna_enc.start_enc_server()
 time.sleep(3.)
 ret4 = antenna_nanten_controller.start_antenna_server()
 
-ret5 = dome_pos.start_dome_pos_server()
-time.sleep(3.)
 ret6 = dome.start_dome_server()
-ret7 = weather.start_weather_server()
 
 ret8 = antenna_enc.enc_client('172.20.0.11',8001)
-ret9 = dome_pos.dome_pos_client('172.20.0.11',8005)
-ret10 = weather.weather_client('172.20.0.11',3001)
-
-p = sub.Popen("python server_stop.py", shell = "True")
-
-ret10.get_weather()
-t = t_w = time.time()
+#ret9 = dome.dome_client('172.20.0.11',8007)
 
 while(1):
 	ret = ret8.get_azel()
-	ret = ret9.dome_encoder_acq()
-	if t-t_w >= 1.0:
-		ret = ret10.get_weather()
-		t_w = time.time()
-	t = time.time()
+	#ret = ret9.dome_encoder_acq()
 	time.sleep(0.005)
 # necrx
-
