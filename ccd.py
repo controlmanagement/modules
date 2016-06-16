af80cace09029ff33bf71d488a1fee03e56451ae
yukihyoto <yhyoto@gmail.com>
2016-06-16 20:47:23 +0900

    Update ccd.py

--- ccd.py
+++ ccd.py
@@ -22,13 +22,13 @@ class ccd_controller(object):
 
 	def oneshot(self, filename, frame_no = 1, size=640*480, Bufferformat='IFIMG_COLOR_RGB24', StartMode = 'IFIMG_DMACAPTURE_START', framenum = 1):
 		# set buffer
-		self.img.set_format(self, frame_no, size, Bufferformat)
+		self.img.set_format(frame_no, size, Bufferformat)
 		#start cap
-		self.img.start_cap(self, frame_no, StartMode)
+		self.img.start_cap(frame_no, StartMode)
 		# get status
-		status = self.img.get_status(self)
+		status = self.img.get_status()
 		# get data
-		self.img.get_data(self, frame_no, framenum, size, dwDataFormat = 'IFIMG_COLOR_RGB24', dwXcoodinates = 0, dwYcoodinates = 0, dwXLength = 640, dwYLength = 480)
+		self.img.get_data(frame_no, framenum, size, dwDataFormat = 'IFIMG_COLOR_RGB24', dwXcoodinates = 0, dwYcoodinates = 0, dwXLength = 640, dwYLength = 480)
 		# save data
 		self.img.save(fiename, size*3, Bufferformat)
 		return status
