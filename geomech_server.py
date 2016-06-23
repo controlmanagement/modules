c303ff1ade8da76847dd4971658a1eb15dca543c
yukihyoto <yhyoto@gmail.com>
2016-06-23 17:54:35 +0900

    Create geomech_server.py

---
+++ geomech_server.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import time
+import geomech
+
+ret0 = geomech.start_geomech_server()
+ret1 = geomech.geomech_client('172.20.0.12',8100)
+
+while(1):
+    ret = ret1.get_geomech_col()
+    geomech_col = ret1.read_geomech()
+    tv = time.time()
+time.sleep(0.1)
