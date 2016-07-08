7ac6d5b3f43bfaf61d7882419a93ee68bc0778b7
H.Iwamura <h.iwamura@a.phys.nagoya-u.ac.jp>
2016-07-08 17:44:34 +0900

    Update server_opt.py

--- server_opt.py
+++ server_opt.py
@@ -4,14 +4,20 @@
 
 # necopt
 
+import time
 import m4
 import abs
+import geomech
 
 ret1 = m4.start_m4_server()
 ret2 = abs.start_abs_server()
+ret3 = geomech.start_geomech_server()
 
+time.sleep(1)
+ret4 = geomech.geomech_client('172.20.0.12',8100)
 
-# necctrl
-
-# necrx
+while(1):
+    ret = ret4.get_geomech_col()
+    print("server_opt.py : main_loop")
+    time.sleep(0.1)
 
