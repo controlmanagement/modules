10fc38e7f321528bf9d7a3102b99dff6f7656226
H.Iwamura <h.iwamura@a.phys.nagoya-u.ac.jp>
2016-07-21 21:16:08 +0900

    Update server_opt.py

--- server_opt.py
+++ server_opt.py
@@ -8,6 +8,7 @@ import time
 import m4
 import abs
 import geomech
+import datetime
 
 ret1 = m4.start_m4_server()
 ret2 = abs.start_abs_server()
@@ -15,9 +16,18 @@ ret3 = geomech.start_geomech_server()
 
 time.sleep(1)
 ret4 = geomech.geomech_client('172.20.0.12',8100)
+date = datetime.date.today()
+b_day = date.day
 
 while(1):
+    date = datetime.date.today()
     ret = ret4.get_geomech_col()
     print("server_opt.py : main_loop")
+    
+    if b_day != date.day:
+        ret4.record_log()
+        b_day = date.day
+    
     time.sleep(0.1)
+    
 
