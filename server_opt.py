153dbe70b56a6a28ed492ae27cdd93c8f98098b1
H.Iwamura <h.iwamura@a.phys.nagoya-u.ac.jp>
2016-08-03 16:48:38 +0900

    Update server_opt.py

--- server_opt.py
+++ server_opt.py
@@ -9,6 +9,7 @@ import m4
 import abs
 import geomech
 import datetime
+import ccd
 
 ret1 = m4.start_m4_server()
 ret2 = abs.start_abs_server()
@@ -18,6 +19,7 @@ time.sleep(1)
 ret4 = geomech.geomech_client('172.20.0.12',8100)
 date = datetime.date.today()
 b_day = date.day
+ret5 = ccd.start_ccd_server()
 
 while(1):
     date = datetime.date.today()
