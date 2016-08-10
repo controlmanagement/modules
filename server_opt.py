fcb52acdea41f192001bd735647a2bbb046e2539
H.Iwamura <h.iwamura@a.phys.nagoya-u.ac.jp>
2016-08-10 14:16:14 +0900

    Update server_opt.py

--- server_opt.py
+++ server_opt.py
@@ -5,6 +5,7 @@
 # necopt
 
 import time
+import m2
 import m4
 import abs
 import geomech
@@ -20,16 +21,30 @@ ret4 = geomech.geomech_client('172.20.0.12',8100)
 date = datetime.date.today()
 b_day = date.day
 ret5 = ccd.start_ccd_server()
+ret6 = m2.start_m2_server()
 
 while(1):
-    date = datetime.date.today()
-    ret = ret4.get_geomech_col()
-    print("server_opt.py : main_loop")
+    try:
+        date = datetime.date.today()
+        ret = ret4.get_geomech_col()
+        print("server_opt.py : main_loop")
+        
+        if b_day != date.day:
+            ret4.record_log()
+            b_day = date.day
+        
+        time.sleep(0.1)
     
-    if b_day != date.day:
-        ret4.record_log()
-        b_day = date.day
-    
-    time.sleep(0.1)
-    
+    except Exception as e:
+        f = open("error.txt","a")
+        timestmp = time.strftime('%Y%m%d_%H%M%S')
+        f.write(timestmp+"\n")
+        f.write('type:' + str(type(e))+"\n")
+        f.write('args:' + str(e.args)+"\n")
+        f.write('message:' + e.message+"\n")
+        f.write(str(e)+"\n")
+        f.write("\n")
+        
+        f.close()
+        
 
