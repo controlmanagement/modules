a6eaaa21ad37c20a9eaa9a8b281cae2c6e739c48
H.Iwamura <h.iwamura@a.phys.nagoya-u.ac.jp>
2016-08-31 13:23:56 +0900

    serverのkill用にpidファイルを作成

--- server_weather.py
+++ server_weather.py
@@ -2,8 +2,16 @@
 #-*- coding: utf-8 -*-
 
 import time
-
+import os
 import weather
+
+
+#for server kill
+pid = os.getpid()
+f = open("server_weather.pid", "w")
+f.write(pid)
+f.close()
+
 
 ret6 = weather.start_weather_server()
 
@@ -12,5 +20,5 @@ time.sleep(.1)
 ret7 = weather.weather_client('172.20.0.11',3001)
 
 while(1):
-	ret = ret7.get_weather()
-	time.sleep(10)
+    ret = ret7.get_weather()
+    time.sleep(10)
