5c6a9b5d1a7e427ea29f6f639fcb3ca78f410cf7
H.Iwamura <h.iwamura@a.phys.nagoya-u.ac.jp>
2016-08-31 13:25:13 +0900

    serverのkill用にファイル作成

--- server_ctrl.py
+++ server_ctrl.py
@@ -18,7 +18,16 @@ import antenna_enc
 import antenna_nanten_controller
 #import dome_pos
 import dome
+import os
 import subprocess as sub
+
+
+#for kill server
+pid = os.getpid()
+f = open("server_ctrl.pid", "w")
+f.write(pid)
+f.close()
+
 
 ret3 = antenna_enc.start_enc_server()
 time.sleep(3.)
@@ -30,9 +39,9 @@ ret8 = antenna_enc.enc_client('172.20.0.11',8001)
 #ret9 = dome.dome_client('172.20.0.11',8007)
 
 while(1):
-	time.sleep(0.1)
-	print('server_ctrl.py : main_loop')
-	ret = ret8.get_azel()
-	#ret = ret9.dome_encoder_acq()
+    time.sleep(0.1)
+    print('server_ctrl.py : main_loop')
+    ret = ret8.get_azel()
+    #ret = ret9.dome_encoder_acq()
 # necrx
 
